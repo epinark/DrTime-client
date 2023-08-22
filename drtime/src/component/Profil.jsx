c011fdc9665205cdf5defbcf459e11e58b1efe13
GaelBass <bassonggael@gmail.com>
2023-08-22 08:26:31 +0200

    correct merge error

--- drtime/src/component/Profil.jsx
+++ drtime/src/component/Profil.jsx
@@ -1,6 +1,29 @@
-import Header from "../../../src/component/Header";
+import React, { useState } from 'react';
+import Header from "./Header";
+import silhouetteProfil from "../assets/img-profil/silhouetteProfil.png";
 export default function Profil() {
-    
+    // document.querySelector('.pp').addEventListener('click',()=>{
+    //     document.querySelector('.pic').style.display=none;
+    //     document.querySelector('.choosePic').style.display=flex;
+    // });
+    const [profileImage, setProfileImage] = useState(silhouetteProfil);
+        const [picVisible, setPicVisible] = useState(true);
+        const [choosePicVisible, setChoosePicVisible] = useState(false);
+      
+        const handleClick = () => {
+          setPicVisible(false);
+          setChoosePicVisible(true);
+        };
+
+    const handleImageChange = (event) => {
+        const selectedImage = event.target.files[0]
+        if (selectedImage) {
+            const imageUrl = URL.createObjectURL(selectedImage);
+            setProfileImage(imageUrl);
+            setPicVisible(false);
+            setChoosePicVisible(true);
+        }
+        ;}
     return (
         <>
             <div>
@@ -15,8 +38,9 @@ export default function Profil() {
                 <p className="text-xl font-bold mb-2">
                     Profile
                 </p>
-                <div className="justify-center rounded-full w-20 h-20 mb-5 cursor-pointer "  >
-                    <img src="../src/assets/img-profil/silhouette-profil.png" alt="Image Silhouette" className="" />
+                <div className="justify-center rounded-full  mb-5 cursor-pointer pp" onClick={handleClick}  >
+                {picVisible &&<img src={silhouetteProfil} alt="Image Silhouette" className="w-40 h-40 pic" />}
+                {choosePicVisible &&<input type="file" className="choosePic " onChange={handleImageChange} />}
                 </div>
                 <p className="profil-name text-xl font-bold mb-10">
                     John Doe
@@ -34,10 +58,11 @@ export default function Profil() {
                 <button className="bg-gradient-to-r from-blue-600 via-blue-700 to-blue-600 rounded-full w-72 h-20 text-3xl text-white mx-auto mb-5 cursor-pointer hover:from-blue-900 ">
               Bearbeiten
             </button>
-            <button className="bg-gradient-to-r from-blue-600 via-blue-700 to-blue-600 rounded-full w-72 h-20 text-3xl text-white mx-auto mb-5 cursor-pointer">
+            <button className="bg-gradient-to-r from-blue-600 via-blue-700 to-blue-600 rounded-full w-72 h-20 text-3xl text-white mx-auto mb-5 cursor-pointer  hover:from-blue-900">
               Zurück
             </button>
             </div>
         </> 
     )
+
 }
